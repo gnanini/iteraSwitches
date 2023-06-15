f3beeac87918aa8553db2598e851a8db3ca1142d
gnanini <gnanini@gmail.com>
2023-06-15 15:37:17 -0300

    filter configuration added, single output file

--- script.py
+++ script.py
@@ -4,29 +4,40 @@ import string
 
 
 def main():
+    # pegar o user e senha
     user = input("Usuário: ")
     senha = getpass(prompt="Senha: ")
+
+    # diretórios e arquivos utilizados
     entradasDir = "entradas"
+    filtrosFile = entradasDir  +"/" + "filtros"
     ipFile = entradasDir + "/" + "ips"
     promptsFile = entradasDir + "/" + "prompts"
     commandsFile = entradasDir + "/" + "comandos"
+    
+    # iterando no arquivo que contém os ips dos switches
     ips = open(ipFile, "r")
+    saida = ""
     for ip in ips.read().split("\n"):
         print(f"Iterando em {ip}")
         try:
-            logFile = open(f"log/{ip}.log", 'w')
             tabTemp = iteraSwitches(ip, user, senha, promptsFile, commandsFile)
             hostname = getHostname(tabTemp)
-            tabTemp = filtraTexto(tabTemp)
-            saida = f"{hostname} - {ip}\n"
+            tabTemp = filtraTexto(tabTemp, filtrosFile)
+            saida += f"{hostname} - {ip}\n"
             for x in tabTemp:
                 for y in x:
-                    saida += y + "\n"
-            print(saida)
-            logFile.write(f"{hostname} - {ip}\n" + saida)
+                    saida += y[:-1] + "\n"
         except:
+            logFile = open(f"log/{ip}.log", 'w')
+            logFile.write(f"{ip}\n")
             print(f"Ip {ip} não acessado")
+            logFile.close()
     ips.close()
+    print(saida)
+    outputFile = open("portasBixadas.txt", 'w')
+    outputFile.write(saida)
+    outputFile.close()
     return
 
 
@@ -41,25 +52,24 @@ def getHostname(texto):
     return "-"
 
 
-
-def filtraTexto(entrada):
+def filtraTexto(entrada, filtrosFile):
     tabTemp = entrada.split("\n")
     saida = []
     k = 0 # contador de condições, se chegar a 2 o input não entra na tabela
+    print("Filtrando o texto")
     for i in range(len(tabTemp)):
         if "GigabitEthernet" in tabTemp[i] and "Administratively" not in tabTemp[i] and 'Description' not in tabTemp[i]:
             temp = [tabTemp[i]]
             for j in range(i+1, len(tabTemp)):
                 if "GigabitEthernet" in tabTemp[j] and 'Description' not in tabTemp[j]:
                     break
-                if validaLinha(tabTemp[j]): 
+                if validaLinha(filtrosFile, tabTemp[j]): 
                     temp.append(tabTemp[j])
                 if ' 0 input errors'  in tabTemp[j] or ' 0 output errors' in tabTemp[j]:
                     k+=1
                 i+=1
             if k < 2:
                 saida.append(temp)
-                #print(temp)
             k = 0
             '''tá salvando temporário as strings filtradas de uma interface à outra, agora é só verificar se tem tudo zerado pra não gravar'''
     return saida
@@ -101,9 +111,12 @@ def iteraSwitches(ip, user, senha, promptsFile, commandsFile):
     return temp
 
 
-def validaLinha(linha):
-    lista = ['Ethernet', 'Untag', 'Tag', 'input', 'error', 'CRC']
-    for x in lista:
+def validaLinha(filtrosFile, linha):
+    f = open(filtrosFile, 'r')
+    filtros = f.read().split('\n')[:-1] # última alteração
+    print(filtros)
+    f.close()
+    for x in filtros:
         if x in linha and "Description" not in linha:
             return True
     return False
